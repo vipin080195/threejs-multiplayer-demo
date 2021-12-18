ce8a29d3866259548e78a7aa657da3db4321f063
zkpranav <zkpranav@icloud.com>
2021-12-18 15:31:51 +0530

    Creating a server integrated with web sockets

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+
+const app = express()
+
+const PORT = 3000
+
+const server = http.createServer(app)
+const io = new Server(server)
+/**
+ * Setup templating engine
+ */
+app.set('views', __dirname + '/views')
+app.engine('.html', require('ejs').__express)
+app.set('view engine', 'ejs')
+
+
+/**
+ * Add middleware to serve static assets
+ */
+app.use('/', express.static(__dirname + '/dist'))
+
+/**
+ * Setup Web Socket events
+ */
+io.on('connection', (socket) => {
+    console.log('New user connected')
+})
+
+
+/**
+ * Setup controllers
+ */
+app.get('/', (req, res) => {
+    res.render('index.html')
+})
+
+/**
+ * Listen for requests
+ */
+ server.listen(PORT, () => {
+    console.log(`Listening on Port: ${PORT}`)
+})
